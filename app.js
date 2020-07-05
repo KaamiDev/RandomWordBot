6f264027bb635b5540eeb87cd81c69dfef34290a
Kaami Dev <kaami@kaamidev.com>
2020-07-05 19:21:25 -0400

    implemented a cron job every 5 mins

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ require('dotenv').config();
 const generateImg = require('./generateImage');
 const getWord = require('./getWord');
 
-getWord().then((word) => {
-	generateImg(word);
+const CronJob = require('cron').CronJob;
+
+console.log('Cron job Staring...');
+const job = new CronJob('*/5 * * * *', function() {
+	getWord().then((word) => {
+		generateImg(word);
+	});
 });
+console.log('Cron job Started. Every 5 minutes.');
+job.start();
