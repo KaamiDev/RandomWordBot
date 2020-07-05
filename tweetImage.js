4de11731bfa31c147878477997223b9e21d58863
Kaami Dev <kaami@kaamidev.com>
2020-07-05 18:20:30 -0400

    setup twit module and tweetimage file

---
+++ tweetImage.js
@@ -0,0 +1,16 @@
+const Twit = require('twit');
+
+var T = new Twit({
+	consumer_key: process.env.CONSUMER_KEY,
+	consumer_secret: process.env.CONSUMER_SECRET,
+	access_token: process.env.ACCESS_KEY,
+	access_token_secret: process.env.ACCESS_SECRET,
+	timeout_ms: 60 * 1000,
+	strictSSL: true
+});
+
+module.exports = () => {
+	T.post('statuses/update', { status: 'My first tweet!' }, function(err, data, response) {
+		console.log(data);
+	});
+};
