fd29c5a9f90f795df1514ee476c90880fa5458e5
Kaami Dev <kaami@kaamidev.com>
2020-07-18 01:25:54 -0400

    made cron job every hour instead of 5 mins

--- app.js
+++ app.js
@@ -5,10 +5,10 @@ const getWord = require('./getWord');
 const CronJob = require('cron').CronJob;
 
 console.log('Cron job Staring...');
-const job = new CronJob('*/5 * * * *', function() {
+const job = new CronJob('0 * * * *', function() {
 	getWord().then((word) => {
 		generateImg(word);
 	});
 });
-console.log('Cron job Started. Every 5 minutes.');
+console.log('Cron job Started. Every 1 hour.');
 job.start();
